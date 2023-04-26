02338cd96f7bdde7bf43d97f719bb182128f51e2
Munsa1 <munsamibenge65@gmail.com>
2023-04-26 04:16:56 +0200

    Add Item Card Structure

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
 function Products() {
   const [data, setData] = useState([]);
@@ -40,8 +42,24 @@ function Products() {
             Electronic's Clothing
           </button>
         </div>
-        {filter.map((product)=>{
-          
+        {filter.map((product) => {
+          return (
+            <>
+              <div className="col-md-3">
+                <Card style={{ width: "18rem" }}>
+                  <Card.Img variant="top" src="holder.js/100px180" />
+                  <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Text>
+                      Some quick example text to build on the card title and
+                      make up the bulk of the card's content.
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                  </Card.Body>
+                </Card>
+              </div>
+            </>
+          );
         })}
       </>
     );
